5d1f5ede00a057395e71c6b09c7e3df73f6d6e98
Lost Diffusion <tassit@gmail.com>
2025-05-30 13:16:40 -0400

    Add partial rot13 solution, though rot13 only works for the alphabet? Weak.

--- python-playground/cipher.py
+++ python-playground/cipher.py
@@ -12,7 +12,8 @@ def convert(msg):
 
 def rot13(plaintext :str):
     ciphertext = ""
-
+    for char in plaintext:
+        ciphertext += chr((ord(char)+13) % 94)
     return ciphertext
 
 def main():
@@ -23,8 +24,13 @@ def main():
         if plaintext != "-1":
             print(f"{Fore.CYAN}[+] The ciphertext is {convert(plaintext)}{Style.RESET_ALL}")
             print(f"{Fore.CYAN}[+] ROT13: {rot13(plaintext)}{Style.RESET_ALL}")
-
-            
+           
     
 if __name__ == "__main__":
+    print(f"{Fore.MAGENTA}++ Assumption: plaintext messages are composed of printable ASCII characters including the upper and lower case English alphabet, numbers from 0-1, and common punctuation.")
+    print("[+] This is the set of printable ASCII characters from chr(32) to chr(126).")
+    print(f"{Style.RESET_ALL}")
+
     main()
+
+    print("\n Thank you for your time. :)\n")
