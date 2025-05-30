8fe9a04c9da25e6900d870b3cd16cdb352fa3fad
Lost Diffusion <tassit@gmail.com>
2025-05-30 15:34:50 -0400

    Fix yara package, add options to the port scanner.

--- python-playground/scanner.py
+++ python-playground/scanner.py
@@ -26,9 +26,34 @@ import string
 import yara
 
 def PrintManual():
-    manual = r'''
+    manual = '''
     Manual:
 
     This tool is a basic port scanner implemented using the Impacket library.
-'''
+    '''
+    print(manual)
 
+def send_UDP_packets():
+    print("\n[+] Preparing to create UDP packets.\n")
+
+def main():
+    # Set up basic flags as per Didier's template, though likely not using many of them.
+    oParserFlag = optparse.OptionParser(usage="\nFlag arguments start with #f#:")
+    oParserFlag.add_option("-l", "--length", action="store_true", default=False, help="Print length of files")
+
+    # Create the main argument parser
+    oParser = optparse.OptionParser(usage="usage: %prog [options]")
+    oParser.add_option("-m", "--man", action="store_true", default=False, help="Print Manual")
+    oParser.add_option("-u", "--UDP", action="store_true", default=False, help="Send UDP packets")
+
+    (options, args) = oParser.parse_args()
+
+    if options.man:
+        PrintManual()
+        return
+    if options.UDP:
+        send_UDP_packets()
+        return
+
+if __name__ == "__main__":
+    main()
