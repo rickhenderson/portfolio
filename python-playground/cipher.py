bd123b241a6b9e7e9498a58c4e340ee56fae9f57
Rick Henderson <4127791+rickhenderson@users.noreply.github.com>
2025-05-30 11:53:50 -0400

    Create cipher.py
    Add the beginning of a simple cipher encryption program.

---
+++ python-playground/cipher.py
@@ -0,0 +1,22 @@
+# Coding interview practice: write a simple cipher encryption program
+# Be able to implment basic ciphers
+
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+from colorama import Fore
+from colorama import Style
+import re
+
+def convert(msg):
+    return "[!] Not yet implemented."
+
+def main():
+    print("[+] Enter '-1' to quit.")
+    plaintext = ""
+    while plaintext != "-1":
+        plaintext = input("Enter a plain text message: ")
+        if plaintext != "-1":
+            print(f"{Fore.CYAN}[+] The ciphertext is {convert(plaintext)}{Style.RESET_ALL}")
+    
+if __name__ == "__main__":
+    main()
