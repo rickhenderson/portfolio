97404b349bd26cd21abf9534fdd3d3b4cc434b91
Lost Diffusion <tassit@gmail.com>
2025-05-30 15:49:42 -0400

    Add a ping feature.

--- python-playground/scanner.py
+++ python-playground/scanner.py
@@ -11,6 +11,11 @@ __date__ = '2025/05/30'
 Source code by Rick Henderson, copyright Rick Henderson.
 Use at your own risk.
 
+Resources:
+  * https://github.com/fortra/impacket/
+  * 
+  * https://denizhalil.com/2025/05/02/what-is-impacket-network-toolkit/
+
 History:
   20250530: Start, based on template style from Didier Stevens.
 
@@ -20,10 +25,12 @@ Todo:
 
 """
 
+from impacket import ImpactPacket, ImpactDecoder
 import optparse
 import re
+import socket
 import string
-import yara
+
 
 def PrintManual():
     manual = '''
@@ -34,7 +41,12 @@ def PrintManual():
     print(manual)
 
 def send_UDP_packets():
-    print("\n[+] Preparing to create UDP packets.\n")
+    print("\n[+] Preparing to create UDP packets.")
+    print("[!] Not yet implemented.\n")
+
+def send_ping():
+    print("\n[+] Preparing to send a ping.")
+    
 
 def main():
     # Set up basic flags as per Didier's template, though likely not using many of them.
@@ -45,15 +57,19 @@ def main():
     oParser = optparse.OptionParser(usage="usage: %prog [options]")
     oParser.add_option("-m", "--man", action="store_true", default=False, help="Print Manual")
     oParser.add_option("-u", "--UDP", action="store_true", default=False, help="Send UDP packets")
+    oParser.add_option("-p", "--ping", action="store_true", default=False, help="Send an ICMP ping")
 
     (options, args) = oParser.parse_args()
-
+    print((options, args))
     if options.man:
         PrintManual()
         return
     if options.UDP:
         send_UDP_packets()
         return
+    if options.ping:
+        if len(args) < 4:
+            print(f"Use: {args[0]} <src ip> <dest ip>")
 
 if __name__ == "__main__":
     main()
