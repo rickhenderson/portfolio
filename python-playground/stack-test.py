38837b1acf81724bd00aa8f4c5abb970c65fe362
Lost Diffusion <tassit@gmail.com>
2025-06-02 18:42:41 -0400

    Add reference link for ADTs.

--- python-playground/stack-test.py
+++ python-playground/stack-test.py
@@ -1,7 +1,7 @@
 # Test Stack class
+# For reference to remember what an ADT was https://www.geeksforgeeks.org/abstract-data-types/
 
 import RDHStack
-
 
 def main():
     print("[+] Test stack Class\n")
