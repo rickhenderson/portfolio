854990442ca037229a99cbb2f8debb2e7125bd18
Lost Diffusion <tassit@gmail.com>
2025-06-03 19:57:38 -0400

    Rename some files to align with Google style guide and pylint.

--- python-playground/stack_test.py
+++ python-playground/stack_test.py
@@ -1,13 +1,17 @@
-# Test Stack class
-# For reference to remember what an ADT was https://www.geeksforgeeks.org/abstract-data-types/
+""" 
+    Test Stack module/program thingy
+    For reference to remember what an ADT was https://www.geeksforgeeks.org/abstract-data-types/
+"""
 
 import RDHStack
 
-def printStack(anyStack):
-    print(anyStack)
+def print_stack(any_stack):
+    """Print the stack and return 1 on success."""
+    print(any_stack)
     return 1
 
 def main():
+    """Testing the stack class."""
     print("[+] Test stack Class\n")
 
     pile = RDHStack.RDHStack()
@@ -18,28 +22,30 @@ def main():
     pile.push(77)
 
     # Display the stack again
-    printStack(pile)
+    print_stack(pile)
 
     # Push a string onto the stack
     print("[+] Push 'add ebx, [result]' onto the stack")
     pile.push("add ebx, [result]")
 
     # Display the stack again
-    printStack(pile)
+    print_stack(pile)
 
     # Remove an item
     print(f"[+] Popped: {pile.pop()}")
-    printStack(pile)
+    print_stack(pile)
 
     print(f"[+] Popped: {pile.pop()}")
     print(f"[+] Popped: {pile.pop()}")
     print(f"[+] Popped: {pile.pop()}")
-    printStack(pile)    
+    print_stack(pile)
+
     print(f"[+] Popped: {pile.pop()}")
     print(f"[+] Popped: {pile.pop()}")
-    printStack(pile)    
+    print_stack(pile)
+
     print(f"[+] Popped: {pile.pop()}")
-    printStack(pile) 
+    print_stack(pile)
 
 if __name__ == "__main__":
     main()
