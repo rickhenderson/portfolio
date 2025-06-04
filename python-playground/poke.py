e658fd56ae88ee5a57077d1ef0eaeb167c2aee6f
Lost Diffusion <tassit@gmail.com>
2025-06-04 16:25:33 -0400

    Add banner, fix arguments.

--- python-playground/poke.py
+++ python-playground/poke.py
@@ -5,6 +5,8 @@ Poke: A simple port poker. I mean... scanner.
 # Created by: Rick Henderson
 # Created on: June 4, 2025
 
+import argparse
+import scapy
 import re
 import sys
 
@@ -15,7 +17,6 @@ def write_file(file_name:str, msg:str)->int:
     with open(f"{file_name}", "wt", encoding="utf-8") as out_file:
         out_file.write(f"{msg}\n")
     return 1
-
 
 def read_file(file_name:str)->str:
     """Read from the file."""
@@ -32,8 +33,38 @@ def read_file(file_name:str)->str:
         print("Poke: Eek! Couldn't read file! I'll have to quit.")
         return None
 
-def main():
+def main(target_host:str):
     """Create a port scanner"""
 
+    banner = """
+    
+ ██▓███   ▒█████   ██ ▄█▀▓█████      ██████  ▄████▄   ▄▄▄       ███▄    █  ███▄    █ ▓█████  ██▀███  
+▓██░  ██▒▒██▒  ██▒ ██▄█▒ ▓█   ▀    ▒██    ▒ ▒██▀ ▀█  ▒████▄     ██ ▀█   █  ██ ▀█   █ ▓█   ▀ ▓██ ▒ ██▒
+▓██░ ██▓▒▒██░  ██▒▓███▄░ ▒███      ░ ▓██▄   ▒▓█    ▄ ▒██  ▀█▄  ▓██  ▀█ ██▒▓██  ▀█ ██▒▒███   ▓██ ░▄█ ▒
+▒██▄█▓▒ ▒▒██   ██░▓██ █▄ ▒▓█  ▄      ▒   ██▒▒▓▓▄ ▄██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒▓██▒  ▐▌██▒▒▓█  ▄ ▒██▀▀█▄  
+▒██▒ ░  ░░ ████▓▒░▒██▒ █▄░▒████▒   ▒██████▒▒▒ ▓███▀ ░ ▓█   ▓██▒▒██░   ▓██░▒██░   ▓██░░▒████▒░██▓ ▒██▒
+▒▓▒░ ░  ░░ ▒░▒░▒░ ▒ ▒▒ ▓▒░░ ▒░ ░   ▒ ▒▓▒ ▒ ░░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░
+░▒ ░       ░ ▒ ▒░ ░ ░▒ ▒░ ░ ░  ░   ░ ░▒  ░ ░  ░  ▒     ▒   ▒▒ ░░ ░░   ░ ▒░░ ░░   ░ ▒░ ░ ░  ░  ░▒ ░ ▒░
+░░       ░ ░ ░ ▒  ░ ░░ ░    ░      ░  ░  ░  ░          ░   ▒      ░   ░ ░    ░   ░ ░    ░     ░░   ░ 
+             ░ ░  ░  ░      ░  ░         ░  ░ ░            ░  ░         ░          ░    ░  ░   ░     
+                                            ░                                                        
+"""
+
+    print(banner)
+
+    print("\n\n[!] Note: Currently only supports scanning one host. Does not accept CIDR notation.")
+    print(f"[+] Target host: {target_host}")
+
+
+
+
+
 if __name__ == "__main__":
-    main()
+    """Should have called it pokey the port scanner. Oh it could have been called porty!"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", help="The IP address of the target host.")
+    args = parser.parse_args()
+
+    # Start the main program and pass in the target IP from the command line.
+    main(args.target)
+
