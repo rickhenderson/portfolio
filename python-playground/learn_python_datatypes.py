c40ba0da38935a75fb8bc0f9741008880bd6611d
Lost Diffusion <tassit@gmail.com>
2025-06-03 20:14:28 -0400

    Add example to teach about data types.

---
+++ python-playground/learn_python_datatypes.py
@@ -0,0 +1,26 @@
+""" 
+    Learn Python Data Types
+"""
+
+# Created by: Rick Henderson
+# Created on: June 3, 2025
+
+class Learner():
+    """A Learner object representing the user or learner."""
+    def __init__(self, learner_name:str):
+        self.learner_name = learner_name
+        self.score = 0
+
+
+def main():
+    """Learn Python Data Types"""
+
+    user_name:str
+    print("\\Piper: Hello! This program will help you learn about Python data types.\n")
+    user_name = input("What is your name? ", end="")
+    print(f"Piper: Hi {user_name}, it's great to meet you! ")
+    print("I'm Piper and I'll be teaching you about the different data types in Python.")
+    print()
+
+if __name__ == "__main__":
+    main()
