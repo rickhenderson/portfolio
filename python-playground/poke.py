bbe9ee7bbf0ca17ccaa63fba9badbf36643c934d
Lost Diffusion <tassit@gmail.com>
2025-06-04 20:21:10 -0400

    Add arping but has a problem.

--- python-playground/poke.py
+++ python-playground/poke.py
@@ -6,7 +6,8 @@ Poke: A simple port poker. I mean... scanner.
 # Created on: June 4, 2025
 
 import argparse
-
+import sys
+from scapy.all import IP, ls, Ether, sr1, UDP, DNS, arping
 
 # Re-use code from previous project but modify it.
 def write_file(file_name: str, msg: str) -> int:
@@ -24,6 +25,7 @@ def read_file(file_name: str) -> str:
                 in_file.readlines()
             )  # Reads the whole file unless the project requires otherwise.
             if dummy:
+
                 return dummy[
                     -1
                 ].strip()  # returns the score from the last line, with no newline.
@@ -32,8 +34,15 @@ def read_file(file_name: str) -> str:
         return -1
     return 1
 
+def perform_arping(targets):
+    """Perform the Scapy arping function to determine which hosts are up using `who-has`."""
+    try:
+        arping(targets)
+    except ValueError as e:
+        print(f"[!] Could not perform arping. Is the destination address correct? {e}")
 
-def main(target_host: str):
+
+def main(args):
     """Create a port scanner"""
 
     banner = """
@@ -53,15 +62,29 @@ def main(target_host: str):
     print(banner)
 
     print(
-        "\n\n[!] Note: Currently only supports scanning one host. Does not accept CIDR notation."
+        "\n[!] Note: Currently only supports scanning one host. Does not accept CIDR notation or domain names."
     )
-    print(f"[+] Target host: {target_host}")
+
+    target_host = args.target
+    if not target_host:
+        print("[!] Can't scan for ports with out a host. Use port.py -t 192.168.1.5.")
+        sys.exit
+    else:
+        print(f"[+] Target host: {target_host}")
+
+    # From https://scapy.readthedocs.io/en/latest/introduction.html#quick-demo
+
+    if args.arping:
+        perform_arping(target_host)
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", help="The IP address of the target host.")
+    parser.add_argument("-ap", "--arping", help="Perform Scapy arping (accepts CIDR)")
     args = parser.parse_args()
+    print(args)
 
     # Start the main program and pass in the target IP from the command line.
-    main(args.target)
+    main(args)
