556ddbe8e161922374af3c32b46dec5a9e7fb4b9
Lost Diffusion <tassit@gmail.com>
2025-06-05 21:49:14 -0400

    Add file for learning even MORE about structures.

---
+++ python-playground/advanced_structures.py
@@ -0,0 +1,11 @@
+"""advanced_structures.py"""
+values = ['34.3', '27.5', '3.14']
+data = map(float, values)
+print(values)
+print(data)
+print(list(data))
+# Len gets the length of a list
+# count will count the number of specified items
+print(len(list(data)))
+
+
